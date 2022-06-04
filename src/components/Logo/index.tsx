b0ca0b3a02d516689c1cf473e27a1393c66d672c
Willian Del Freu <williankeyex@gmail.com>
2022-06-04 00:31:32 -0300

    Project finish

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { LogoProps } from 'types/api'
-import { getImageUrl } from 'utils/getImageUrl'
+import { value Image } from 'types/api'
+import { value getImageUrl } from 'utils/getImageUrl'
 import * as S from './styles'
 
-const Logo = ({ url, alternativeText }: LogoProps) => (
+const Logo = ({ url, alternativeText }: Image) => (
   <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 )
 
